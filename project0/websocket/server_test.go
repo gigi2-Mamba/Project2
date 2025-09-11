c8f46c1953db766eccd1e3d03fe51558e0415a80
0xpayjay <gigi2kb24@163.com>
2025-09-11 10:45:44 +0800

    single p2p ws

--- project0/websocket/server_test.go
+++ project0/websocket/server_test.go
@@ -2,8 +2,10 @@ package websocket
 
 import (
 	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func TestServer(t *testing.T) {
@@ -16,20 +18,55 @@ func TestServer(t *testing.T) {
 			writer.Write([]byte("upgrade error"))
 			return
 		}
+		ws := &Ws{conn}
 		// You should continuously receive data from  the conn.
 		go func() {
-			for {
-				typ, msg, err := conn.ReadMessage()
-				if err != nil {
-					return
-				} // type有三种类型，TextMessag,BinaryMessage,CloseMessage   pingmessage, pongmessage 对应心跳
+			ws.ReadCycle()
+		}()
 
+		go func() {
+			ticker := time.NewTicker(time.Second)
+			for now := range ticker.C {
+				ws.Write("来自服务端的数据: " + now.String())
 			}
 		}()
+
 	})
+	http.ListenAndServe(":8086", nil)
 }
 
-// 封装会好看点
+// 封装会好看点,没有对比啊
 type Ws struct {
 	conn *websocket.Conn
+}
+
+func (w *Ws) Write(msg string) {
+	err := w.conn.WriteMessage(websocket.TextMessage, []byte(msg))
+	if err != nil {
+		log.Println("write error: ", err.Error())
+	}
+
+}
+
+func (w *Ws) ReadCycle() {
+	conn := w.conn
+	for {
+
+		typ, msg, err := conn.ReadMessage() // 怎么读取信息，ws是依赖连接对象
+		if err != nil {
+			// exit loop
+			// record log
+			return
+		} // type有三种类型，TextMessag,BinaryMessage,CloseMessage   pingmessage, pongmessage 对应心跳
+		switch typ {
+		case websocket.CloseMessage:
+			conn.Close()
+			return
+		case websocket.BinaryMessage, websocket.TextMessage:
+			log.Println("read message: ", string(msg))
+		default:
+			// no operation
+
+		}
+	}
 }
